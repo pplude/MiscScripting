dc9bb88971705d671aa2df7e473bdaa91872fea7
Patrick Plude <pplude@me.com>
2016-03-23 09:58:16 -0400

    Create BAT-Admin-Snip.bat
    This snippet is placed at the header of a batch file to force admin rights using UAC.

---
+++ BAT-Admin-Snip.bat
@@ -0,0 +1,28 @@
+@echo off
+
+REM THIS SECTION CHECKS FOR ADMIN RIGHTS AND CALLS UAC IF NEEDED
+:-------------------------------------
+REM Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM If error flag set, we do not have admin
+IF '%ERRORLEVEL%' NEQ '0' (
+	echo Requesting administrative privilages...
+	goto UACPrompt
+) ELSE ( GOTO gotAdmin )
+
+REM Create a vbs that calls UAC
+:UACPrompt
+	echo Set UAC = CreateObject^("Shell.Application"^) > "%TEMP%\getadmin.vbs"
+	set params = %*:"=""
+	echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%TEMP%\getadmin.vbs"
+	
+	"%TEMP%\getadmin.vbs"
+	del "%TEMP%\getadmin.vbs"
+	exit /B
+
+REM Continue	
+:gotAdmin
+	pushd "%CD%"
+	CD /D "%~dp0"
+:-------------------------------------
